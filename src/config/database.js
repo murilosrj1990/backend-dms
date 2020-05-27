f03143285f5cb85a899b7ff9c8b0f396f11e2ff6
set <set>
2020-05-27 00:11:23 -0300

    database in cloud, filter return users

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'postgres',
-    database: 'appdrmylena',
+    host: 'tuffi.db.elephantsql.com',
+    username: 'ixtqkzad',
+    password: 'yG1bLw42OPrXZc5Sm3gj3dJpMqBjmtHk',
+    database: 'ixtqkzad',
     define: {
         timestamps: true,
         underscored: true,
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,7 +2,9 @@ const User = require('../models/User');
 
 module.exports = {
     async index(req,res){
-        const users = await User.findAll();
+        const users = await User.findAll({
+            attributes: ['name','email']
+        });
 
         res.json(users);
     },
