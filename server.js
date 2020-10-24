88e28305ef1cd20b8b77d85e19a4ae1e663ec42f
Murilo Sérgio R. Jr <murilo.cc.uepb@gmail.com>
2020-10-24 14:58:30 -0300

    fix port listen for Heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const routes = require('./router');
 
 const app = express();
-
+    
 require('./src/database');
 
 app.disable('x-powered-by');
@@ -19,4 +19,4 @@ app.use(cors());
 app.use(routes);
 
 
-app.listen(process.env.API_PORT);
+app.listen(process.env.API_PORT || process.env.PORT);
