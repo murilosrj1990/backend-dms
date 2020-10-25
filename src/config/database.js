2ae13f69191e1250d902301d75fa09c8e7153e96
Murilo Sérgio R. Jr <murilo.cc.uepb@gmail.com>
2020-10-25 01:10:40 -0300

    fix to heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: "ec2-52-86-116-94.compute-1.amazonaws.com",
-    username: "ikotzcobvacwdd",
-    password: "f7028913dda8711eb09904d6a5f3230d10b6ec92cc6fc2be83e09ee1dbd9f97f",
-    database: "dbfo046hoapb0l",
+    host: 'ec2-52-86-116-94.compute-1.amazonaws.com',
+    username: 'ikotzcobvacwdd',
+    password: 'f7028913dda8711eb09904d6a5f3230d10b6ec92cc6fc2be83e09ee1dbd9f97f',
+    database: 'dbfo046hoapb0l',
     define: {
         timestamps: true,
         underscored: true,
